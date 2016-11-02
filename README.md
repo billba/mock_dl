42dcc23ee6e036e1032c8c3e53c850a3c91c60db
billba <billba@microsoft.com>
2016-11-02 16:30:20 +1300

    it works a little

---
+++ README.md
@@ -0,0 +1,26 @@
+# mock_dl
+
+## Test your DirectLine v3 clients using this mock
+
+0. clone this repo
+
+1. Optionally, create a .env file containing defaults 
+
+    PORT = your_favorite_port
+
+2. Configure
+
+    npm install
+
+3. Build & run
+
+    npm run build (or watch)
+    npm run start (or nodewatch)
+
+4. Aim your DirectLine v3 client at this, e.g.
+
+    http://localhost:{your_favorite_port}?segment={test_case}
+
+where `test_case` is one of the test cases currently listed in index.ts
+
+
---
+++ index.ts
@@ -0,0 +1,102 @@
+require('dotenv').config();
+
+import * as express from 'express';
+import bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, PATCH, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  next();
+});
+
+app.post('/:test/tokens/generate', (req, res) => {
+    res.send({
+        conversationId: "abc123",
+        token: req.params.test,
+        expires_in: 1800 
+    });
+});
+
+app.post('/:test/tokens/refresh', (req, res) => {
+    res.send({
+        conversationId: "abc123",
+        token: req.params.test,
+        expires_in: 1800 
+    });
+});
+
+app.post('/:test/conversations', (req, res) => {
+    res.send({
+        conversationId: "abc123",
+        token: req.params.test,
+        expires_in: 1800,
+        streamUrl: "http://notworkingyet"
+    });
+    sendMessage(res, "Welcome to MockBot!");
+});
+
+interface Activity {
+    type: string,
+    text?: string,
+    id?: string,
+    from?: { id?: string, name?: string }
+}
+
+let messageId = 0;
+const queue:Activity[] = [];
+
+const sendMessage = (res: express.Response, text: string) => {
+    queue.push({
+        type: "message",
+        text
+    })
+}
+
+app.post('/:test/conversations/:conversationId/activities', (req, res) => {
+    const id = messageId++;
+    res.send({
+        id: id.toString(),
+    });
+
+    sendMessage(res, `echo of post #${id}: ${req.body.text}`);
+});
+
+app.post('/:test/conversations/:conversationId/upload', (req, res) => {
+    const id = messageId++;
+    res.send({
+        id: id.toString(),
+    });
+});
+
+app.get('/:test/conversations/:conversationId/activities', (req, res) => {
+    if (queue.length > 0) {
+        let msg = queue.shift();
+        let id = messageId++;
+        msg.id = id.toString();
+        msg.from = { id: "id", name: "name" };
+        res.send({
+            activities: [msg],
+            watermark: id.toString()
+        });
+    } else {
+        res.send({
+            activities: [],
+            watermark: messageId.toString()
+        })
+    }
+});
+
+app.listen(process.env.port || process.env.PORT || 3000, () => {
+    console.log('listening');
+});
+
+/* Here are the supported test cases:
+
+works: everything works perfectly
+
+*/
