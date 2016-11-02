e8e697beb48bdbc1e6fc938677d8e8e62e7a3fdb
billba <billba@microsoft.com>
2016-11-02 07:50:32 -0700

    test & area support

--- index.js
+++ index.js
@@ -11,29 +11,41 @@ app.use((req, res, next) => {
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     next();
 });
-app.post('/:test/tokens/generate', (req, res) => {
+const timeout = 60 * 1000;
+const conversationId = "mockversation";
+const token = "your_token_here";
+const expires_in = 1800;
+const streamUrl = "http://nostreamsupport";
+app.post('/:test/:area/tokens/generate', (req, res) => {
     res.send({
-        conversationId: "abc123",
-        token: req.params.test,
-        expires_in: 1800
+        conversationId,
+        token,
+        expires_in
     });
 });
-app.post('/:test/tokens/refresh', (req, res) => {
+app.post('/:test/:area/tokens/refresh', (req, res) => {
     res.send({
-        conversationId: "abc123",
-        token: req.params.test,
-        expires_in: 1800
+        conversationId,
+        token,
+        expires_in
     });
 });
-app.post('/:test/conversations', (req, res) => {
-    res.send({
-        conversationId: "abc123",
-        token: req.params.test,
-        expires_in: 1800,
-        streamUrl: "http://notworkingyet"
-    });
-    sendMessage(res, "Welcome to MockBot!");
+app.post('/:test/:area/conversations', (req, res) => {
+    if (req.params.test === 'timeout' && req.params.area === 'start') {
+        setTimeout(() => startConversation(req, res), timeout);
+        return;
+    }
+    startConversation(req, res);
 });
+const startConversation = (req, res) => {
+    res.send({
+        conversationId,
+        token,
+        expires_in,
+        streamUrl
+    });
+    sendMessage(res, `Welcome to MockBot! Here is test ${req.params.test} on area ${req.params.area}`);
+};
 let messageId = 0;
 const queue = [];
 const sendMessage = (res, text) => {
@@ -42,20 +54,53 @@ const sendMessage = (res, text) => {
         text
     });
 };
-app.post('/:test/conversations/:conversationId/activities', (req, res) => {
+app.post('/:test/:area/conversations/:conversationId/activities', (req, res) => {
+    if (req.params.test === 'expire' && req.params.area === 'post') {
+        res.status(403).send({ error: { code: "TokenExpired" } });
+        return;
+    }
+    if (req.params.test === 'timeout' && req.params.area === 'post') {
+        setTimeout(() => postMessage(req, res), timeout);
+        return;
+    }
+    postMessage(req, res);
+});
+const postMessage = (req, res) => {
     const id = messageId++;
     res.send({
-        id: id.toString(),
+        id,
     });
     sendMessage(res, `echo of post #${id}: ${req.body.text}`);
+};
+app.post('/:test/:area/conversations/:conversationId/upload', (req, res) => {
+    if (req.params.test === 'expire' && req.params.area === 'upload') {
+        res.status(403).send({ error: { code: "TokenExpired" } });
+        return;
+    }
+    if (req.params.test === 'timeout' && req.params.area === 'upload') {
+        setTimeout(() => upload(req, res), timeout);
+        return;
+    }
+    upload(req, res);
 });
-app.post('/:test/conversations/:conversationId/upload', (req, res) => {
+const upload = (req, res) => {
     const id = messageId++;
     res.send({
-        id: id.toString(),
+        id,
     });
+};
+app.get('/:test/:area/conversations/:conversationId/activities', (req, res) => {
+    if (req.params.test === 'expire' && req.params.area === 'get') {
+        res.status(403).send({ error: { code: "TokenExpired" } });
+        return;
+    }
+    if (req.params.test === 'timeout' && req.params.area === 'get') {
+        setTimeout(() => getMessages(req, res), timeout);
+        return;
+    }
+    getMessages(req, res);
 });
-app.get('/:test/conversations/:conversationId/activities', (req, res) => {
+const getMessages = (req, res) => {
     if (queue.length > 0) {
         let msg = queue.shift();
         let id = messageId++;
@@ -63,21 +108,23 @@ app.get('/:test/conversations/:conversationId/activities', (req, res) => {
         msg.from = { id: "id", name: "name" };
         res.send({
             activities: [msg],
-            watermark: id.toString()
+            watermark: id
         });
     }
     else {
         res.send({
             activities: [],
-            watermark: messageId.toString()
+            watermark: messageId
         });
     }
-});
+};
 app.listen(process.env.port || process.env.PORT || 3000, () => {
     console.log('listening');
 });
 /* Here are the supported test cases:
 
-works: everything works perfectly
+works/all: everything works perfectly. Enjoy.
+expire/[post|get|upload]: operation fails due to expired token
+timeout/[start|post|get|upload]: operation takes 60 seconds to respond
 
 */
--- index.ts
+++ index.ts
@@ -14,31 +14,46 @@ app.use((req, res, next) => {
   next();
 });
 
-app.post('/:test/tokens/generate', (req, res) => {
+const timeout = 60*1000;
+const conversationId = "mockversation";
+const token = "your_token_here";
+const expires_in = 1800;
+const streamUrl = "http://nostreamsupport";
+
+app.post('/:test/:area/tokens/generate', (req, res) => {
     res.send({
-        conversationId: "abc123",
-        token: req.params.test,
-        expires_in: 1800 
+        conversationId,
+        token,
+        expires_in 
     });
 });
 
-app.post('/:test/tokens/refresh', (req, res) => {
+app.post('/:test/:area/tokens/refresh', (req, res) => {
     res.send({
-        conversationId: "abc123",
-        token: req.params.test,
-        expires_in: 1800 
+        conversationId,
+        token,
+        expires_in 
     });
 });
 
-app.post('/:test/conversations', (req, res) => {
-    res.send({
-        conversationId: "abc123",
-        token: req.params.test,
-        expires_in: 1800,
-        streamUrl: "http://notworkingyet"
-    });
-    sendMessage(res, "Welcome to MockBot!");
+app.post('/:test/:area/conversations', (req, res) => {
+    if (req.params.test === 'timeout' && req.params.area === 'start') {
+        setTimeout(() => startConversation(req, res), timeout);
+        return;
+    }
+
+    startConversation(req, res);
 });
+
+const startConversation = (req: express.Request, res: express.Response) => {
+    res.send({
+        conversationId,
+        token,
+        expires_in,
+        streamUrl
+    });
+    sendMessage(res, `Welcome to MockBot! Here is test ${req.params.test} on area ${req.params.area}`);
+}
 
 interface Activity {
     type: string,
@@ -57,23 +72,65 @@ const sendMessage = (res: express.Response, text: string) => {
     })
 }
 
-app.post('/:test/conversations/:conversationId/activities', (req, res) => {
+app.post('/:test/:area/conversations/:conversationId/activities', (req, res) => {
+    if (req.params.test === 'expire' && req.params.area === 'post') {
+        res.status(403).send({ error: { code: "TokenExpired" } });
+        return;
+    }
+
+    if (req.params.test === 'timeout' && req.params.area === 'post') {
+        setTimeout(() => postMessage(req, res), timeout);
+        return;
+    }
+
+    postMessage(req, res);
+});
+
+const postMessage = (req: express.Request, res: express.Response) => {
     const id = messageId++;
     res.send({
-        id: id.toString(),
+        id,
     });
 
     sendMessage(res, `echo of post #${id}: ${req.body.text}`);
+}
+
+app.post('/:test/:area/conversations/:conversationId/upload', (req, res) => {
+    if (req.params.test === 'expire' && req.params.area === 'upload') {
+        res.status(403).send({ error: { code: "TokenExpired" } });
+        return;
+    }
+
+    if (req.params.test === 'timeout' && req.params.area === 'upload') {
+        setTimeout(() => upload(req, res), timeout);
+        return;
+    }
+
+    upload(req, res);
 });
 
-app.post('/:test/conversations/:conversationId/upload', (req, res) => {
+const upload = (req: express.Request, res: express.Response) => {
     const id = messageId++;
     res.send({
-        id: id.toString(),
+        id,
     });
+}
+
+app.get('/:test/:area/conversations/:conversationId/activities', (req, res) => {
+    if (req.params.test === 'expire' && req.params.area === 'get') {
+        res.status(403).send({ error: { code: "TokenExpired" } });
+        return;
+    }
+
+    if (req.params.test === 'timeout' && req.params.area === 'get') {
+        setTimeout(() => getMessages(req, res), timeout);
+        return;
+    }
+
+    getMessages(req, res);
 });
 
-app.get('/:test/conversations/:conversationId/activities', (req, res) => {
+const getMessages = (req: express.Request, res: express.Response) => {
     if (queue.length > 0) {
         let msg = queue.shift();
         let id = messageId++;
@@ -81,15 +138,15 @@ app.get('/:test/conversations/:conversationId/activities', (req, res) => {
         msg.from = { id: "id", name: "name" };
         res.send({
             activities: [msg],
-            watermark: id.toString()
+            watermark: id
         });
     } else {
         res.send({
             activities: [],
-            watermark: messageId.toString()
+            watermark: messageId
         })
     }
-});
+}
 
 app.listen(process.env.port || process.env.PORT || 3000, () => {
     console.log('listening');
@@ -97,6 +154,8 @@ app.listen(process.env.port || process.env.PORT || 3000, () => {
 
 /* Here are the supported test cases:
 
-works: everything works perfectly
+works/all: everything works perfectly. Enjoy.
+expire/[post|get|upload]: operation fails due to expired token
+timeout/[start|post|get|upload]: operation takes 60 seconds to respond 
 
 */
